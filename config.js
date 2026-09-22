7848646bfd282f5874e013a0f90f1c511fe2bcab
only_ahmedo <kndkhe9g38d@gmail.com>
2026-09-22 20:16:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 window.RET_CONFIG = Object.freeze({
-  // Public WispByte backend used by the RET panel.
-  // Never put the Discord bot token or OAuth client secret here.
   API_BASE: "http://78.154.103.42:9975",
   WEB_ORIGIN: "https://kndkhe9g38d-ctrl.github.io",
   SUPPORT_URL: "https://discord.gg/SHgz77ZUkM",
